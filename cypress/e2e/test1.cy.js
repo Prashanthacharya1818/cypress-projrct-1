e979ba66c22ca64438061e6595aa09c89da25884
Prashanthacharya1818 <prashanthacharya0to9@gmail.com>
2023-07-25 12:41:04 +0530

    update 1

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -2,5 +2,5 @@
 it('Google Search', () => {
     cy.visit('https://google.com')
     cy.get('#APjFqb').type('automation step by step{enter}')
-    //cy.get('.FPdoLc > center > .gNO89b').click()
+    cy.get('.FPdoLc > center > .gNO89b').click()
 })
