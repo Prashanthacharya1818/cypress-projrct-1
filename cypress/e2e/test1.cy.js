3e7cf144290f934a2cc91d0ce53b34621c1f0a42
Prashanthacharya1818 <prashanthacharya0to9@gmail.com>
2023-07-25 13:07:18 +0530

    changed dC timeout to 5000

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -2,5 +2,9 @@
 it('Google Search', () => {
     cy.visit('https://google.com')
     cy.get('#APjFqb').type('automation step by step{enter}')
-    cy.get('.FPdoLc > center > .gNO89b').click()
+    //cy.get('.FPdoLc > center > .gNO89b').click()
+    //cy.get('.eKjLze > .g > [lang="en"] > .tF2Cxc > .yuRUbf > a > .LC20lb').click()
+    //cy.contains('Automation Step by step:NEVER STOP LEARNING').click()
+    //cy.get('#xXe_ZKrDAZL04-EPkOi-0Ag__9 > .zItAnd > .O3S9Rb').click()
+    cy.contains('Images').click()
 })
