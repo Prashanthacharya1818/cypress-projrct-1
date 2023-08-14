cbaa51b6342472d104321881341eed7a3308b6f3
Prashanthacharya1818 <prashanthacharya0to9@gmail.com>
2023-08-14 18:24:45 +0530

    added changs

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,11 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
-  
- e2e: {
+module.exports = defineConfig({ 
+  e2e: {
     projectId: "gw7pna",
     watchForFileChanges: false,
     defaultCommandTimeout: 5000,
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on, config){
       // implement node event listeners here
     },
   },
